4c64060380306b10582bb0d22b8ef39994f45262
Kaile Su <s2179956@staff.ad.griffith.edu.au>
2024-04-22 15:57:29 +1000

    signalTest

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { signal, computed } from '@angular/core';
+
 
 @Component({
   selector: 'app-tab1',
@@ -10,5 +12,32 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class Tab1Page {
-  constructor() {}
+
+  // Create a signal for the array of products
+ products = signal([
+  { name: 'Product A', price: 10 },
+  { name: 'Product B', price: 15 },
+  { name: 'Product C', price: 20 },
+ ]);
+ // Create a computed signal for total value based on products
+  totalValue = computed(() =>
+  this.products().reduce((sum:number, product: any) => sum + product.price, 0)
+ );
+ //reading the signal
+ 
+  constructor() {
+
+    console.log("computed2",this.totalValue());  Output: 45
+  }
+
+  addContact(){
+    let n = prompt("Name");
+    let p = Number(prompt("Price"));
+    this.products.update((val:any)=>[...val, { name: n, price: p }])
+
+    
+
+
+    
+  }
 }
